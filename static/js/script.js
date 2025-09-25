f62874d03819e2138198edc52ff404105403067e
guna0223 <gunasekar0223@gmail.com>
2025-09-25 09:30:31 +0530

    file added

--- static/js/script.js
+++ static/js/script.js
@@ -4,77 +4,88 @@ const products = [
         description: "To take pic",
         price: 20000,
         img: "camer.jpg",
-        discount: 40000
+        discount: 40000,
+        off: 50
     },
     {
         title: "Mobile ",
         description: "To paly game",
         price: 20000,
         img: "mobile.jpg",
-        discount: 40000
+        discount: 40000,
+        off: 50
     },
     {
         title: "Pro",
         description: "To show big screen",
         price: 20000,
         img: "pro.jpg",
-        discount: 40000
+        discount: 40000,
+        off: 50
     },
     {
         title: "Samsung mobile",
         description: "To use camera",
         price: 20000,
         img: "s24.jpg",
-        discount: 40000
+        discount: 40000,
+        off: 50
     },
     {
         title: "TV",
         description: "To see movie in big screen",
         price: 20000,
         img: "tv.jpg",
-        discount: 40000
+        discount: 40000,
+        off: 50
     },
     {
         title: "TWS",
         description: "Enjoy the sound",
         price: 20000,
         img: "tws.jpg",
-        discount: 40000
+        discount: 40000,
+        off: 50
     },
     {
         title: "vivo",
         description: "Enjoy mobile",
         price: 20000,
         img: "vivo.jpg",
-        discount: 40000
+        discount: 40000,
+        off: 50
     },
     {
         title: "vr",
         description: "Enjoy vr ",
         price: 20000,
         img: "vr.jpg",
-        discount: 40000
+        discount: 40000,
+        off: 50
     },
     {
         title: 'Pilgrim',
         description: 'Advanced Hari Growth Serum',
         price: 297,
         img: 'serum.webp',
-        discount: 400
+        discount: 400,
+        off: 50
     },
     {
         title: 'Nerf 2.0',
         description: 'Elite 2.0 Commander',
         price: 917,
         img: 'gun.jpeg',
-        discount: 1500
+        discount: 1500,
+        off: 50
     },
     {
         title: 'COSCO',
         description: 'Robona pro football',
         price: 645,
         img: 'football.jpeg',
-        discount: 1000
+        discount: 1000,
+        off: 50
     },
 
 ]
@@ -85,11 +96,13 @@ products.forEach(product => {
     col.className = 'col-12 col-sm-6 col-md-4 col-lg-3'
     col.innerHTML = `
          <div class="card">
-            <img src="static/img/product/${product.img}" class="card-img-top">
+            <img src="static/img/product/${product.img}" class="card-img-top imgs">
               <div class="card-body">
                   <h5 class="card-title"> ${product.title} </h5>
                     <p class="card-text">${product.description} </p>
-                    <p><span style="text-decoration: line-through; color: gray;"> Rs:${product.discount}</span> <strong style="color: black; text-decoration: none;> <span style="color: black; text-decoration: none;"> Rs:${product.price}</span></p>
+                    <p><del><span style="color: gray;">Rs:${product.discount}</span></del
+                    <small><sup style="color:red">${product.off}%</sup></small>
+                    <strong style="color: black; text-decoration: none;> <span style="color: black; text-decoration: none;"> Rs:${product.price}</span></p>
                         <button class=" btn btn-secondary text-light">Add to cart</button>
                 </div>
          </div>
